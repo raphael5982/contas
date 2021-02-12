76c5282d54aff00ff362a5a19b6ff0a08375b84c
Raphael <raphaelvictor40@gmail.com>
2021-02-11 21:55:59 -0300

    Aritimetrica

---
+++ R/Aritimetrica.R
@@ -0,0 +1,64 @@
+#' Function somar
+#'
+#' this function is good to sum.
+#'
+#' @param a first number.
+#' @param b Second number.
+#'
+#' @examples
+#' somar(1, 4)
+#'
+#' @export
+somar <- function(a, b) {
+  sum(a, b)
+
+}
+
+#' Function subtrair
+#'
+#' this function is good to sub.
+#'
+#' @param a first number.
+#' @param b Second number.
+#'
+#' @examples
+#' subtrair(1, 4)
+#'
+
+
+#' @export
+subtrair <- function(a,b){
+  a - b
+}
+
+#' Function multiplicar
+#'
+#' this function is good to mult.
+#'
+#' @param a first number.
+#' @param b Second number.
+#'
+#' @examples
+#' multiplicar(1, 4)
+#'
+
+#' @export
+multiplicar <- function(a,b){
+  a * b
+}
+
+#' Function divisao
+#'
+#' this function is good to div.
+#'
+#' @param a first number.
+#' @param b Second number.
+#'
+#' @examples
+#' divisao(1, 4)
+#'
+
+#' @export
+divisao <- function(a,b){
+  a / b
+}
